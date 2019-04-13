d2d86660e25065062c6928ff530540f58ed92bd9
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-13 11:29:31 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,7 +10,7 @@ def argue(phrase = "I'm right and you are wrong!")
   phrase
 end
 
-def greeting(greeting = "Hi there, ", name = "Bobby!")
+def greeting(greeting, name)
   puts "#{greeting}, #{name}"
 end
 
